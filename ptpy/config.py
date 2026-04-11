ed67151df2e9b36399214c9637555138642e0fe8
Jakub Heicl <heicljakub3@gmail.com>
2026-04-11 14:26:15 +0200

    fix

--- ptpy/config.py
+++ ptpy/config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+PACKAGE_ROOT = Path(__file__).resolve().parent
+
 INPUT_FOLDER: Path = Path("input_files")
 REPOSITORY_FOLDER: Path = Path("repository")
 RUN_FOLDER: Path = Path("runs")
-BASES_FOLDER: Path = Path("bases")
+BASES_FOLDER: Path = Path(PACKAGE_ROOT, "bases")
 
 LANL_EXTENSION = "lanl"
 DZ_EXTENSION = "dz" 
